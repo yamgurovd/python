34199fa8f6606fc62c79cb0fa5315bdb3bd1784a
dayan <yamgurovd@gmail.com>
2024-10-30 17:47:41 +0300

    OOP Выполнил задачи 6-8 блока 1_3

---
+++ stepic/oop/1_3/task_7.py
@@ -0,0 +1,27 @@
+"""
+Подвиг 9. Объявите класс с именем Figure и двумя атрибутами:
+
+type_fig: 'ellipse'
+color: 'red'
+Создайте экземпляр с именем fig1 этого класса и добавьте в него следующие локальные атрибуты:
+
+start_pt: (10, 5)
+end_pt: (100, 20)
+color: 'blue'
+Удалите из экземпляра класса свойство color и выведите на экран список всех локальных свойств (без значений)
+объекта fig1 в одну строчку через пробел в порядке, указанном в задании.
+"""
+
+
+class Figure:
+    type_fig = 'ellipse'
+    color = 'red'
+
+fig1 = Figure()
+setattr(fig1, "start_pt", (10, 5))
+setattr(fig1, "end_pt", (100, 20))
+setattr(fig1, "color", 'blue')
+
+
+delattr(fig1, "color")
+print(*fig1.__dict__)
