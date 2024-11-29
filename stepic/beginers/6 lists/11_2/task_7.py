743ca5e8d1900ec67779ab0532e73818d391a699
d <yamgurovd@gmail.com>
2024-11-29 22:54:12 +0300

    Begin решил задачи 1-8 блока 11_2

---
+++ stepic/beginers/6 lists/11_2/task_7.py
@@ -0,0 +1,8 @@
+"""Дополните приведенный код так, чтобы он вывел "перевёрнутый" список languages
+(т.е. элементы будут идти в обратном порядке).
+
+Примечание. Для вывода списка воспользуйтесь функцией print()."""
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese',
+             'Lahnda']
+
+print(list(reversed(languages)))
