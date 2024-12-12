18823870d2c4a7062244acfa4c4bbc0d7d201322
d <yamgurovd@gmail.com>
2024-12-12 18:34:18 +0300

    Middle блоки 3_1 и 3_2

--- scripts.py
+++ scripts.py
@@ -1,3 +1,5 @@
+from operator import index
+
 from src.randomizers import randomizer_person
 
 # print(randomizer_person.generate_first_name_female())
@@ -31,7 +33,24 @@ hotels = [
      ]
 ]
 
-page = 6
+page = 7
 per_page = 1
 
-print(hotels[page:][:per_page])
+# print(hotels[page:][:per_page])
+
+for i in range(len(hotels)):
+    # print(i)
+    if i == 7:
+        # print(hotels[i])
+        for j in range(len(hotels[i])):
+            print(hotels[i][j])
+
+lst1 = 'sdfjksdf'
+print(lst1.strip())
+
+
+numbers = [-6, -8, 0, 1, 3, 8, -7, 12, 17, 24, 25, 3, 5, 1]
+res = 0
+for num in numbers:
+    res += (num % 2 == 1) and (num > 1)
+print(res)
