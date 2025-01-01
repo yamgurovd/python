cea5a7d9958b41a6836b581131031d830b0cbd66
d <yamgurovd@gmail.com>
2025-01-01 21:52:24 +0300

    Middle Разбор задач 1 - блоков 6_2 и 4_6

---
+++ stepic/middle/4 tuple/6_2/task_1.py
@@ -0,0 +1,5 @@
+"""Используя индексацию кортежа, дополните приведенный ниже код так, чтобы переменная last содержала
+последний элемент кортежа countries."""
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+last = countries[-1]
+print(last)
